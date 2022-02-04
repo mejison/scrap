c3283fe0f962ace1f232adf3641e9df0da0ca9d5
mejison <dev.mejison@gmail.com>
2022-02-04 22:34:42 +0200

    add to envs

--- app/Http/Controllers/UnityController.php
+++ app/Http/Controllers/UnityController.php
@@ -12,8 +12,8 @@ class UnityController extends Controller
     public $csrf;
 
     public function __construct() {
-        $this->bearer = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjk5OTI2NiwiaWF0IjoxNjQzODI2MzQ2LCJqdGkiOiJhNjdkZDhmYi1iYmZjLTRiMjQtYjdkNi1jZjEyOTJhOGI2NzUifQ.rNTxlmg3G9ZzpVZos2nIFNBpWYMLDZ5Gt6FYyPWhJJU';
-        $this->csrf = 'vC3vDCuuGVOzuFgeueaolR-No2oeCaOnalix30OX4QLnEXvVAXeoXGeil_a_9Uj7a_mL7H9Q_JDKHXjzeoYR7A';
+        $this->bearer = env('BEARER', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjk5OTI2NiwiaWF0IjoxNjQzODI2MzQ2LCJqdGkiOiJhNjdkZDhmYi1iYmZjLTRiMjQtYjdkNi1jZjEyOTJhOGI2NzUifQ.rNTxlmg3G9ZzpVZos2nIFNBpWYMLDZ5Gt6FYyPWhJJU');
+        $this->csrf = env('CSRF', 'vC3vDCuuGVOzuFgeueaolR-No2oeCaOnalix30OX4QLnEXvVAXeoXGeil_a_9Uj7a_mL7H9Q_JDKHXjzeoYR7A');
     }
 
     public function get() {
