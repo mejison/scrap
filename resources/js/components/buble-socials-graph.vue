77a2318f71cdaa89c8d826f9ef2b24667324d794
mejison <dev.mejison@gmail.com>
2022-02-22 12:31:45 +0200

    change followers list

--- resources/js/components/buble-socials-graph.vue
+++ resources/js/components/buble-socials-graph.vue
@@ -1,7 +1,20 @@
 <template>
     <div>
         <ul>
-            <li v-if="item.attributes.reach.blog">
+             <li v-for="(rate, index) in item.rates" :key="index">
+                 <a :href="rate.attributes.property_url" target="_blank">
+                    <i class="lni lni-blogger" v-if="rate.attributes.publishable_type == 'BlogProperty'"></i> 
+                    <i class="lni lni-facebook" v-if="rate.attributes.publishable_type == 'FacebookProperty'"></i> 
+                    <i class="lni lni-instagram" v-if="rate.attributes.publishable_type == 'InstagramProperty'"></i> 
+                    <i class="lni lni-pinterest" v-if="rate.attributes.publishable_type == 'PinterestProperty'"></i> 
+                    <i class="lni lni-twitch" v-if="rate.attributes.publishable_type == 'TwitchProperty'"></i> 
+                    <i class="lni lni-wordpress" v-if="rate.attributes.publishable_type == 'WordpressProperty'"></i> 
+                    <i class="lni lni-youtube" v-if="rate.attributes.publishable_type == 'YoutubeProperty'"></i> 
+                    <i class="lni lni-twitter"  v-if="rate.attributes.publishable_type == 'TwitterProperty'"></i>
+                    {{ humanformat(rate.attributes.reach) }}
+                 </a>
+            </li>
+            <!-- <li v-if="item.attributes.reach.blog">
                 <i class="lni lni-blog" ></i> {{ humanformat(item.attributes.reach.blog) }}
             </li>
             <li v-if="item.attributes.reach.facebook">
@@ -24,7 +37,7 @@
             </li>
             <li v-if="item.attributes.reach.youtube">
                 <i class="lni lni-youtube" ></i> {{ humanformat(item.attributes.reach.youtube) }}
-            </li>
+            </li> -->
         </ul>
     </div>
 </template>
